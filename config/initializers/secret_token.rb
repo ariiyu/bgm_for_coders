b395bc0a7ddfe3ec289d4e7045cbdd10a4d1a885
ariiyu <yusuke.ary@gmail.com>
2013-11-09 19:26:42 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BgmForCoders::Application.config.secret_key_base = '11f59eb33cd6eabeda39d3f48b5721f81be3f8ac60f69df27e372d74be3cdb240a8d0651e7176a0a2ff3e4378d9e8d4dce306e6f299a733d9f853c30048ea71b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BgmForCoders::Application.config.session_store :cookie_store, key: '_bgm_for_coders_session'
